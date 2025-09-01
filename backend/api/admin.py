090a74a96271a4be466fbe99f601d073bc08cf98
Sohail007x <sohailt7x@gmail.com>
2025-09-02 02:53:37 +0530

    backend done

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models import Note
 
 # Register your models here.
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['id','title','content','created_at','author']
---
+++ backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from api.views import NoteListCreate, NoteDelete
+
+urlpatterns = [
+    path("notes/", NoteListCreate.as_view(), name='note-list'),
+    path('notes/delete/<int:pk>/', NoteDelete.as_view(), name='delete-note'),
+]
